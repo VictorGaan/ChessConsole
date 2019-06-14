06eab56caa13681c259f38b8e10be8a6e9bf23de
Victor <gaan_v@bk.ru>
2019-06-14 17:13:33 +0700

    Chess

---
+++ ChessConsole/Chess/Chess.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Chess
+    {
+        //Forsyth–Edwards Notation
+        public string fen { get; private set; }
+        static DateTime dt = DateTime.Now;
+        Board board;
+        Moves moves;
+        List<FigureMoving> allMoves;
+        public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        {
+            this.fen = fen;
+            board = new Board(fen);
+            moves = new Moves(board);
+            FindAllMoves();
+        }
+        Chess(Board board)
+        {
+            this.board = board;
+            this.fen = board.fen;
+            moves = new Moves(board);
+
+        }
+        public Chess Move(string move)
+        {
+            FigureMoving fm = new FigureMoving(move);
+            if (!moves.CanMove(fm))
+                return this;
+            Board nextBoard = board.Move(fm);
+            Chess nextChess = new Chess(nextBoard);
+            return nextChess;
+        }
+        //Положение фигуры на доске
+        public char GetFigureAt(int x, int y)
+        {
+            TimerFigure();
+            Square square = new Square(x, y);
+            Figure f = board.GetFigureAt(square);
+            return f == Figure.none ? '.' : (char)f;
+        }
+        void TimerFigure()
+        {
+            if (board.moveColor == Color.white)
+            {
+                TimerCallback timeCB = new TimerCallback(PrintTime);
+                Timer time = new Timer(timeCB, null, 0, 1000);
+            }
+            else if(board.moveColor==Color.black)
+            {
+                TimerCallback timeCB = new TimerCallback(PrintTime);
+                Timer time = new Timer(timeCB, null, 0, 1000);
+            }
+        }
+        static void PrintTime(object state)
+        {
+            DateTime dateTime = DateTime.Now;
+            var timeNow = dateTime - dt;
+            if (timeNow.Seconds > 300)
+            {
+                Environment.Exit(0);
+            }
+        }
+        void FindAllMoves()
+        {
+            allMoves = new List<FigureMoving>();
+            foreach (FigureOnSquare fs in board.YieldFigures())
+                foreach (Square to in Square.YieldSquares())
+                {
+                    FigureMoving fm = new FigureMoving(fs, to);
+                    if (moves.CanMove(fm))
+                        if (!board.IsCheskAfterMove(fm))
+                            allMoves.Add(fm);
+                }
+        }
+        public List<string> GetAllMoves()
+        {
+            FindAllMoves();
+            List<string> list = new List<string>();
+            foreach (FigureMoving fm in allMoves)
+                list.Add(fm.ToString());
+            return list;
+        }
+        public bool IsCheck()
+        {
+            return board.IsCheck();
+        }
+    }
+}
---
+++ ChessConsole/ChessApp/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Chess;
+
+
+namespace ChessApp
+{
+    class Program
+    {
+
+        private static void Main()
+        {
+            Console.WriteLine("\t\t\t\t\t\t\tConsole Chess");
+            AIChess();
+        }
+        private static void AIChess()
+        {
+
+            Random random = new Random();
+            Chess.Chess chess = new Chess.Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+            List<string> list;
+            while (true)
+            {
+                list = chess.GetAllMoves();
+                Console.WriteLine(chess.fen);
+                Print(ChessToAscii(chess));
+                Console.WriteLine(chess.IsCheck() ? "Шах" : "");
+                foreach (string moves in chess.GetAllMoves())
+                    Console.Write(moves + "\n");
+                Console.WriteLine();
+                Console.Write("< ");
+                string move = Console.ReadLine();
+                if (move == "q") break;
+                try
+                { if (move == "") move = list[random.Next(list.Count)]; }
+                catch (Exception) { }
+                chess = chess.Move(move);
+            }
+        }
+
+        static string ChessToAscii(Chess.Chess chess)
+        {
+            Console.Clear();
+            string text = "    a b c d e f g h\n";
+            text += "  +-----------------+\n";
+            for (int y = 7; y >= 0; y--)
+            {
+                text += y + 1;
+                text += " | ";
+                for (int x = 0; x < 8; x++)
+                {
+                    text += chess.GetFigureAt(x, y) + " ";
+                }
+                text += "|\n";
+            }
+            text += "  +-----------------+\n";
+            text += "    a b c d e f g h\n";
+            return text;
+
+        }
+        private static void Print(string text)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            foreach (char x in text)
+            {
+                if (x >= 'a' && x <= 'z')
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                else if (x >= 'A' && x <= 'Z')
+                    Console.ForegroundColor = ConsoleColor.White;
+                else
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write(x);
+            }
+            Console.ForegroundColor = color;
+        }
+
+    }
+}
